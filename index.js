71db345975a2796dd5c28961906cb46a91653717
Reid <Reid@pokersprout.com>
2018-05-20 10:52:31 -0400

    webpush code
    webpush code to create a Subscribe Route
    
    Create subscribe route: request body, send 201 status, create
    (optional) payload to send in notification and pass the object into the
    sendNotification method and catch any errors.

--- index.js
+++ index.js
@@ -6,5 +6,25 @@ const config = require './config';
 
 const app = express();
 
+app.use(bodyParser.json());
+
 const publicVapidKey = config.publicVapidKey;
 const privateVapidKey = config.privateVapidKey;
+
+webpush.setVapidDetails('mailto:test@test.com', publicVapidKey, privateVapidKey);
+
+// Subscribe Route
+app.post('/subscribe', (req, res) => {
+  // Get pushSubscription object
+  const subscription = req.body;
+
+  // Send 201 - resource created
+  res.status(201).json({});
+
+  // Create payload (this is optional, but using the title of notication as payload)
+  const payload = JSON.stringify({ title: 'Push Test' });
+
+  // Pass the object into the sendNotifcation function
+  webpush.sendNotifcation(subscription, payload).catch(err => console.error(err));
+
+});
